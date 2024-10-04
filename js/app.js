e0101ec53a8a0e4f936bd947d4108356674f8b5c
Edwin Antonio Sion Tepe <tepe1600@gmail.com>
2024-10-04 11:23:34 -0700

    Es el primer commit donde se sube le proyecto manipulando el DOM con Javascript

---
+++ js/app.js
@@ -0,0 +1,13 @@
+let contenedor = document.getElementById("root");
+
+let nombre = "Antonio";
+let edad = 27;
+
+contenedor.innerHTML = `
+                        <table>
+                            <tr>
+                                <td>Titulo de la primera fila</td>
+                                <th>Cuerpo</th>
+                            <tr>
+                        </table>
+                        `;
